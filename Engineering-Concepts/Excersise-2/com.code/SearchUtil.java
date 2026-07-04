c008e2011ea71ccc5453836482550ed7fb916e50
Quid15k <arushpc.15k@gmail.com>
2026-07-04 08:56:01 +0000

    solved Exercise-2

---
+++ Engineering-Concepts/Excersise-2/com.code/SearchUtil.java
@@ -0,0 +1,32 @@
+public class SearchUtil {
+    
+    public static Product linearSearch(Product[] products, String targetId) {
+        for (Product product : products) {
+            if (product.getProductId().equals(targetId)) {
+                return product; 
+            }
+        }
+        return null;
+    }
+
+    
+    public static Product binarySearch(Product[] sortedProducts, String targetId) {
+        int left = 0;
+        int right = sortedProducts.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int comparison = sortedProducts[mid].getProductId().compareTo(targetId);
+
+            if (comparison == 0) {
+                return sortedProducts[mid]; 
+            }
+            if (comparison < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return null;
+    }
+}
